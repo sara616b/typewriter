98a1dc191dc76761ae7d92eee9897765004bbbae
Sarah <sara616b@stud.kea.dk>
2021-02-09 12:10:45 +0100

    javascript works - writes text one letter at a time

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,32 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+let text;
+let showtext;
+let container = document.querySelector("h1");
+let i = 1;
+
+function init() {
+  console.log("init");
+
+  //get text
+  text = container.textContent;
+
+  //remove text
+  container.innerHTML = "";
+
+  writetext();
+}
+
+function writetext() {
+  console.log("writetext");
+
+  //loop - show n'th letter, time, increment, more letters?
+  showtext = text.substring(0, i);
+  container.textContent = showtext;
+  if (i < text.length) {
+    i++;
+    setTimeout(writetext, 300);
+  }
+}
